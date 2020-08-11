5682480c1d1c6fd5c218485baed476cdb299daca
yn-20 <yn-20@LAPTOP-0L5V98GT>
2020-08-11 14:55:30 +0900

    자바 1일차 예제

---
+++ doitjava/src/doitjava/Hello2.java
@@ -0,0 +1,12 @@
+package doitjava;
+
+public class Hello2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("헬로우");
+		System.out.println("헬로우2");
+		//이글은 주석이다.
+	}
+
+}
