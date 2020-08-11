11382c1fc913885058db8326a0821ad7eace89f3
byenggeol <byenggeol.dev@gmail.com>
2020-08-12 01:06:01 +0900

    집에서대충 만저봄

--- doitjava/src/doitjava/Gittest2.java
+++ doitjava/src/doitjava/Gittest2.java
@@ -5,6 +5,7 @@ public class Gittest2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("두번째 커밋 테스트");
+			System.out.println("오우노우!");
 	}
 
 }
