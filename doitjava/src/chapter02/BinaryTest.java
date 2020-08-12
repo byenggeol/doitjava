47a6aabd679464cf5bd1ecb4ee7e3b62f4058c41
byenggeol <byenggeol.dev@gmail.com>
2020-08-12 15:26:50 +0900

    가즈아

--- doitjava/src/chapter02/BinaryTest.java
+++ doitjava/src/chapter02/BinaryTest.java
@@ -4,7 +4,7 @@ public class BinaryTest {
 
 	public static void main(String[] args) {
 		int bNum = 0B1010;  //2진수 (0B를붙여야한다)
-		int dNum = 10;		//10진수 
+		int dNum = 10;		//10진수 (노)
 		int oNum = 012;		//8진수	(0을붙이면 8진수)
 		int hNum = 0xA;		//16진수 (0x를붙여야 16진수로)
 		
