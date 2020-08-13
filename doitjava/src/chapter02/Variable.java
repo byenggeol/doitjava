b5a3790ea14ed6b20bdd33bbe65f722042fb1e5b
byenggeol <byenggeol.dev@gmail.com>
2020-08-13 13:13:43 +0900

    변수다

---
+++ doitjava/src/chapter02/Variable.java
@@ -0,0 +1,22 @@
+package chapter02;
+
+public class Variable {
+
+public static void main(String[] args) {
+	int level = 99; //레벨 변수를 선언하고 정수10을저장하였습니다.
+		int hp;
+		System.out.println("현재 레벨은 얼마인가요?");
+		System.out.println(level);
+		System.out.println("현재 체력은 얼마인가요?");
+		hp = 50000;
+		System.out.println(hp);
+		System.out.println("오우거로부터 39500의 데미지를 입었습니다.");
+		hp = 10500;
+		System.out.println("현재 체력 게이지는 얼마인가요?");
+		System.out.println(hp);
+		
+		
+
+	}
+
+}
